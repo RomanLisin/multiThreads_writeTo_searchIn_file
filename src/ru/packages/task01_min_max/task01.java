2caf252bd17a1bb06100bce16413ef854591e8cc
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2026-01-15 22:52:48 +1000

    Add Threads Task01

--- src/ru/packages/task01_min_max/task01.java
+++ src/ru/packages/task01_min_max/task01.java
@@ -4,12 +4,23 @@ import ru.packages.OperationManager;
 void main() {
     InputManager inputManager = new InputManager();
     OperationManager operationManager = new OperationManager();
-    
+
     int[] array;
 
     array = inputManager.getIntArray();
-    System.out.println("Максимальное число из введенных: " + operationManager.getMax(array));
-    System.out.println("Минимальное число из введенных: " + operationManager.getMin(array));
+
+    Runnable max = () -> {
+        System.out.println("Максимальное число из введенных: " + operationManager.getMax(array));
+    };
+    Runnable min = () -> {
+        System.out.println("Минимальное число из введенных: " + operationManager.getMin(array));
+    };
+
+    Thread maxThrd = new Thread(max);
+    Thread minThrd = new Thread(min);
+
+    maxThrd.start();
+    minThrd.start();
 }
 
 
